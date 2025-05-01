f9528f5d09b657eb1e9cd4fed882fd801960c617
Doug Banks <doug.banks@gmail.com>
2025-05-01 10:02:29 -0700

    thrifting

---
+++ Html.Thrifting/javascript/gameInfo.js
@@ -0,0 +1,10 @@
+// For stuff like rules variants, etc.
+define(["dojo/domReady!"], function () {
+  var maxPlayers = 8;
+  var minPlayers = 2;
+
+  return {
+    maxPlayers: maxPlayers,
+    minPlayers: minPlayers,
+  };
+});
